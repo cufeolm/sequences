a4d803a1173ee0fa3d6f214d79731e33cceaf79a
mostafa552 <mostafa_552@yahoo.com>
2020-03-17 19:56:16 +0200

    Update tb_pkg.sv

--- tb_pkg.sv
+++ tb_pkg.sv
@@ -16,65 +16,46 @@
 package tb_pkg;
    import uvm_pkg::*;
    `include "uvm_macros.svh"
-   
+   //declarations
+   typedef enum logic [31:0] { 
+      LSB=32'b11xxxxx001001xxxxx1xxxxxxxxxxxxx,
+      LUW=32'b0000000100xxxxxxxxxxxxxxxxxxxxxx     // (NOP)
+      }opcode;
+  opcode si_a [] ; 
+  integer supported_instructions ; 
+
+   //includes 
    `include "cmd_sequence_item.sv"
-   `include "leon_sequence_item.sv"
-   `include "leon_sequence.sv"
+   //`include "leon_sequence_item.sv"
+   `include "generic_sequence.sv"
    //`include "mips_package.sv"
    `include "driver.sv"
    `include "test.sv"
+   
+
+  function void fill_si_array( );// fill supported instruction array 
+      // this does NOT  affect generalism this makes sure you dont run 
+      // the same function twice in a test bench 
+      `ifndef SET_UP_INSTRUCTION_ARRAY
+      `define SET_UP_INSTRUCTION_ARRAY
+          opcode si_i ; // for iteration only
+          supported_instructions = si_i.num() ;
+          si_a=new [supported_instructions] ; 
+          
+          si_i = si_i.first();
+          for (integer i=0 ; i < supported_instructions ; i++ )
+          begin   
+              si_a [i]= si_i ; 
+              si_i=si_i.next();
+              
+          end 
+          //$display("array is filled and ready to use");
+      `endif  
+  endfunction
+
+
+  
 /*
-   function cmd_sequence_item get_format (cmd_sequence_item k);
-      leon_seq_item ay;
-      ay.op = k.inst[31:30];
-         case (ay.op)
-            2'b01 : 
-                  //call format1
-                  ay.disp30 = k.inst[29:0];
-            2'b00 : begin
-                     ay.op2 = k.inst[24:22];
-                     case (ay.op2)
-                        3'b100,3'b000 :
-                        //sethi & no op & unimplemnted format 2
-                           begin
-                              ay.rd = k.inst[29:25];
-                              ay.imm22 = k.inst[21:0];
-                           end
-                        3'b010, 3'b110, 3'b111 :
-                        //branch & fp branch & co branch format 2
-                           begin
-                              ay.a = k.inst[29];
-                              ay.cond = k.inst[28:25];
-                              ay.disp22 = k.inst[21:0];
-                           end
-                        default: uvm_report_error("k.instruction", "k.instruction format not defined");
-                     endcase
-                  end
-            2'b10, 2'b11 : begin
-                           ay.i = k.inst[13];
-                           ay.rd = k.inst[29:25];
-                           ay.op3 = k.inst[24:19];
-                           ay.rs1 = k.inst[18:14];
-                           if (ay.i)
-                           //format 3 register register
-                              begin	
-                                 ay.asi = k.inst[12:5];
-                                 ay.rs2 = k.inst[4:0];
-                              end
-                           else
-                           //format 3 register immediate
-                              begin
-                                 ay.imm13 = k.inst[12:0];
-                              end
-                           
-                        end
-            default: uvm_report_error("k.instruction", "k.instruction format not defined");
-         endcase
-         if (!($cast(ay,k))) 
-            $fatal(1,"failed to cast transaction to leon's transaction"); 
-         return k;
-      endfunction 
-*/
 function cmd_sequence_item get_format (logic [31:0] inst);
    leon_seq_item ay;
    cmd_sequence_item k ; 
@@ -130,5 +111,6 @@ function cmd_sequence_item get_format (logic [31:0] inst);
          $fatal(1,"failed to cast transaction to leon's transaction"); 
       return k;
    endfunction 
+*/
 endpackage : tb_pkg
    
