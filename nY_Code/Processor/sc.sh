6d56463bc4f5b9941c7dccb38af72bd032f9b0ca
mostafa552 <mostafa_552@yahoo.com>
2020-03-19 00:06:17 +0200

    i ll put it in a stand aloe repository

--- nY_Code/Processor/sc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#read file
-#$(cat /dev/null > dut.f)
-cat dut.f | while read line ;do
-#firstletters=$( set -f; printf "%c" $line )
-#echo "$line" >> hhhh.txt
-$(printf "./processor/" >> dut.f)
-$(printf "$line\n" >> dut.f)
-done
--- nY_Code/run.do
+++
@@ -1,8 +0,0 @@
-vlog -f DUT.f
-# vlog Processor/stg_final.v
-# vsim stg_final
-vlog top.sv -timescale=1ps/1ps
-vlog processor_interface.sv -timescale=1ps/1ps
-vsim top +UVM_TESTNAME=processor_test
-
-run -all
