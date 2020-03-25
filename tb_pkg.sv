d4f5e34c22fc233a5211cf58a7f0aafbba1d2068
Mostafa Ayman <62121720+mostafa552@users.noreply.github.com>
2020-03-25 05:31:28 +0200

    Update tb_pkg.sv

--- tb_pkg.sv
+++ tb_pkg.sv
@@ -13,7 +13,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-just a test
+
 package tb_pkg;
    import uvm_pkg::*;
    `include "uvm_macros.svh"
